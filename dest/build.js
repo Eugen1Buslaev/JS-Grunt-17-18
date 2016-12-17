3077e69db1444f380575dec195b6a815344238e1
Your Name <evgenij.buslaev@gmail.com>
2016-12-17 15:35:01 +0200

    ad

---
+++ dest/build.js
@@ -0,0 +1,15 @@
+(function(){
+	var funcOne = function(){
+		var tree = "oak";
+	};
+
+	console.log('one');
+	console.log('tree');
+})();
+(function(){
+	var funcOne = function(){
+		var two = 2;
+	};
+
+	console.log('two');
+})();
